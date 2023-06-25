1d242aa74a0e6cdcec099c6b36b9cf09d998b79a
HekalMaulana <116002257+HekalMaulana@users.noreply.github.com>
2023-06-25 19:07:51 +0800

    data off coffe machin

---
+++ coffe_machine_data.py
@@ -0,0 +1,39 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+COIN = {
+    "QUARTER" : 0.25,
+    "DIME" : 0.10,
+    "NICKEL" : 0.05,
+    "PENNY" : 0.01,
+}
+
